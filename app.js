4910ee935bce6c4a51dfde7e21a332b15493994c
Aniket Sharma <tpbc1717@gmail.com>
2025-02-20 13:57:10 +0530

    Added local frontend url in cors origin

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 dotenv.config({path: "./config/config.env"})
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
 }))
